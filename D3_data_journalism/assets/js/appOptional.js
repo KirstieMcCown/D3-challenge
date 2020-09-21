d3743d087354f02b19b9a2c301f8a4b046b10783
Kirstie McCown <accounts@intimemechanical.com.au>
2020-09-21 21:31:16 +0800

    working on further axes

---
+++ D3_data_journalism/assets/js/appOptional.js
@@ -0,0 +1,193 @@
+// @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 700;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";  
+
+// function used for updating x-scale var upon click on axis label
+function xScale(data, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+      d3.max(data, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  var label;
+
+  if (chosenXAxis === "poverty") {
+    label = "In Poverty (%)";
+  }
+  else if (chosenXAxis === "age") {
+    label = "Age (Median)";
+  }
+  else {
+    label = "Household Income (Median)";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.state}<br>${d[chosenXAxis]}<br>${d[chosenYAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  return circlesGroup;
+}
+
+
+
+
+
+// Load data from data.csv
+d3.csv("../D3_data_journalism/assets/data/data.csv").then(function(data) {
+
+    // Print the Data to the Console
+    // console.log(data);
+
+    //  Parse Data/Cast as numbers
+    data.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+      data.age = +data.age;
+      data.income = +data.income;
+      data.smokes = +data.smokes;
+      data.obesity = +data.obesity;
+    });
+
+    // Create scale functions
+    var xLinearScale = d3.scaleLinear()
+      .domain([8, d3.max(data, d => d.poverty + 2)])
+      .range([0, chartWidth]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([2, d3.max(data, d => d.healthcare + 2)])
+      .range([chartHeight, 0]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Append Axes to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Create Circles
+    
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "10")
+      .attr("fill", "#3288bd")
+      .attr("opacity", ".5");
+
+    // Add abbreviation labels to circles
+    var circleLabels = chartGroup.selectAll(null).data(data).enter().append("text");
+      
+    circleLabels
+        .attr("x", function(d) {
+            return xLinearScale(d.poverty);
+        })
+        .attr("y", function(d) {
+            return yLinearScale(d.healthcare);
+        })
+        .text(function(d) {
+            return d.abbr;
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("text-anchor", "middle")
+        .attr("fill", "white");
+
+    // Create axis labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left)
+        .attr("x", 0 - (chartHeight - 130))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .attr("font-family", "sans-serif")
+        .style('stroke', '#000')
+        .text("Lacks Healthcare (%)");
+
+  chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth - 325}, ${chartHeight + chartMargin.top - 10})`)
+        .attr("class", "axisText")
+        .attr("font-family", "sans-serif")
+        .style('stroke', '#000')
+        .text("In Poverty (%)");
+
+    });
